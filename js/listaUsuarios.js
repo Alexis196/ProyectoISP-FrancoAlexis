0c978bc080094c5c44533d03a267d3703b1fd6a9
Alexis Franco <ronaldofranco196@gmail.com>
2022-11-08 20:55:14 -0300

    nuevas modificaciones

--- js/listaUsuarios.js
+++ js/listaUsuarios.js
@@ -12,22 +12,15 @@ const clasificar = document.getElementById('megas');
 
 // Evento
 let enviar = document.getElementById('enviar');
-let boton = document.getElementById('generar');
 
-enviar.addEventListener('click', (e)=>{
+enviar.addEventListener('click', (e)=>
+{
     e.preventDefault();
     let persona = new Usuario(inU.value,inD.value,parseInt(inT.value),inC.value,megas.value);
     usuarios.push(persona);
     console.log(usuarios);
-    const parrafo = document.querySelector('.parrafoDiv2');
-    parrafo.innerHTML = 'Hemos recibido tus datos. Pronto nos contactaremos contigo'
-})
-
-generar.addEventListener('click', (e)=>
-{
-    e.preventDefault();
     usuarios = localStorage.setItem('cliente',(JSON.stringify(usuarios)));
-    console.log(usuarios);
     const parrafo = document.querySelector('.parrafoDiv2');
-    parrafo.innerHTML = 'Nuevo Usuario generado con éxito!'
+    parrafo.innerHTML = 'Hemos recibido tus datos. Pronto nos contactaremos contigo';
+    
 })
