26e543524b380273d442299cdb1e13665ce759b4
Alexis Franco <ronaldofranco196@gmail.com>
2022-11-13 20:57:18 -0300

    sweet Alert

--- js/listaUsuarios.js
+++ js/listaUsuarios.js
@@ -10,21 +10,31 @@ const clasificar = document.getElementById('megas');
 
 
 
-// Evento
+// Eventos
 let enviar = document.getElementById('enviar');
+let confirma = document.querySelector('.btnConfirma');
 
 enviar.addEventListener('click', (e)=>
 {
     e.preventDefault();
     let persona = new Usuario(inU.value,inD.value,parseInt(inT.value),inC.value,megas.value);
     usuarios.push(persona);
-    console.log(usuarios);
     localStorage.setItem('cliente',(JSON.stringify(usuarios)));
     let descarga = JSON.parse(localStorage.getItem('cliente'));
     console.log(descarga);
+    if (boton2 != 0){
+        let text = document.querySelector('.textoConfirma');
+        text.innerHTML= `El costo de la instalación es de $3500. <br>
+        El servicio solicitado (${usuarios[0].megas}) lo pagarás del 01 al 10 del siguiente mes.`;
+    }
+})
+
+
+confirma.addEventListener('click', (e)=>{
+    e.preventDefault();
     Swal.fire({
-        title:`Muchas gracias ${usuarios[0].nombre}`,
-        text:'Pronto nos contactaremos contigo.',
+        title: `Muchas Gracias ${usuarios[0].nombre}`,
+        text: 'Pronto nos contactaremos contigo para coordinar día y hora de la instalación',
         icon: 'success',
         footer: 'SERVICIO DE INTERNET',
     })
